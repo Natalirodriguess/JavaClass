6c135554680265b74d39408bf3ef13161abc716c
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2022-09-19 01:20:00 -0300

    Tratamento de excecoes

--- src/br/com/erros/DividePorZero.java
+++ src/br/com/erros/DividePorZero.java
@@ -1,9 +1,30 @@
 package br.com.erros;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DividePorZero {
     public static void main (String [] args){
+        try {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite um numero para o numerador: ");
+        int numero1 = input.nextInt();
+
+        System.out.println("Digite um numero para o denominador: ");
+        int numero2 = input.nextInt();
+
+        System.out.println(numero1/numero2);
+        }
+        catch (InputMismatchException e_1){
+            System.out.println("");
+
+        }
+        catch (Throwable e_2){
+            System.out.println("");
+        }
+
+
+
 
     }
 }
